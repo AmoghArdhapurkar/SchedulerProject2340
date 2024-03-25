f15a4cbbb42a569c1a2a9ffed57aef148dfb8cec
AmoghArdhapurkar <amogh.ardhapurkar@gmail.com>
2024-03-25 16:19:02 -0400

    Test Commit

--- app/src/main/java/com/example/appredo/MainActivity.java
+++ app/src/main/java/com/example/appredo/MainActivity.java
@@ -15,7 +15,7 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
+//Test
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
